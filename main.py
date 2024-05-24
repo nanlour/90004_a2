f9e41e89cd471493785b397e6cd78fc0b4090e5a
nanlour <tjuwrr@gmail.com>
2024-05-24 22:26:51 +1000

    Add commemts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from args import args
 
 
 class MuscleFiber:
+    # Initial MuscleFiber
     def __init__(self):
         self.max_size = 4
         self.anabolic_hormone = 50
@@ -17,17 +18,21 @@ class MuscleFiber:
 
         self.fiber_size = (0.2 + random.random() * 0.4) * self.max_size
 
+    # Regulate nutrient, set upper limit of nutrient store for each cell.
     def __regulate_nutrient(self):
         self.nutrient = min(MuscleModel.nutrient_max, self.nutrient)
 
+    # Each day MuscleFiber will get constant amount of nutrient
     def get_nutrient(self):
         self.nutrient += MuscleModel.nutrient
         self.__regulate_nutrient()
 
+    # Perform daily activity that affects hormone levels.
     def perform_daily_activity(self):
         self.catabolic_hormone += 2.0 * math.log10(self.fiber_size)
         self.anabolic_hormone += 2.5 * math.log10(self.fiber_size)
 
+    # Exercise promotes hormone secretion
     def lift_weights(self):
         if random.random() < (MuscleModel.intensity / 100) ** 2:
             self.catabolic_hormone += math.log10(self.fiber_size) * 44
@@ -38,6 +43,7 @@ class MuscleFiber:
         self.catabolic_hormone -= math.log10(self.catabolic_hormone) * 0.5 * sleep_hours
         self.anabolic_hormone -= math.log10(self.anabolic_hormone) * 0.48 * sleep_hours
 
+    # Regulate Hormones for gien upper and lower bound
     def regulate_hormones(self):
         self.anabolic_hormone = min(
             MuscleModel.anabolic_hormone_max, self.anabolic_hormone
@@ -56,6 +62,7 @@ class MuscleFiber:
         self.__grow()
         self.__regulate_muscle_fibers()
 
+    # Simulate muscle grow
     def __grow(self):
         fiber_size_change = 0.20 * min(
             math.log10(self.anabolic_hormone), 1.05 * math.log10(self.catabolic_hormone)
@@ -65,6 +72,7 @@ class MuscleFiber:
             self.nutrient -= fiber_size_change * 5000
         self.fiber_size += fiber_size_change
 
+    # Regulate muscle fibers for gien upper and lower bound
     def __regulate_muscle_fibers(self):
         self.fiber_size = max(1, self.fiber_size)
         self.fiber_size = min(self.max_size, self.fiber_size)
@@ -102,6 +110,7 @@ class MuscleModel:
             [MuscleFiber() for _ in range(self.width)] for _ in range(self.height)
         ]
 
+    # Generate normally distributed value for given mean and std_dev.
     def generate_normal(mean, std_dev):
         u1 = random.random()
         u2 = random.random()
@@ -110,6 +119,7 @@ class MuscleModel:
 
         return mean + z * std_dev
 
+    # Get eight neighbors of given position.
     def __get_neighbors(self, x, y):
         return [
             ((y - 1) % self.height, (x - 1) % self.width),
@@ -122,6 +132,7 @@ class MuscleModel:
             (y, (x + 1) % self.width),
         ]
 
+    # Diffusion hormone
     def __diffuse(self):
         new_values_anabolic = [
             [0 for _ in range(self.width)] for _ in range(self.height)
@@ -160,6 +171,7 @@ class MuscleModel:
                     y
                 ][x]
 
+    # Get normally distributed sleep hours based on given argument
     def getSleepHours():
         sleep_hours = -1
         while sleep_hours < 0 or sleep_hours > MuscleModel.hours_of_sleep * 2:
@@ -168,7 +180,9 @@ class MuscleModel:
             )
         return sleep_hours
 
+    # One step for one day
     def step(self):
+        # Calculate statistical data
         self.muscle_mass = (
             sum(
                 muscle_fiber.fiber_size
@@ -187,6 +201,8 @@ class MuscleModel:
             for row in self.muscle_fiber_grid
             for muscle_fiber in row
         ) / (self.height * self.width)
+
+        # Add data to record
         self.data.append(
             [
                 self.time,
@@ -196,6 +212,7 @@ class MuscleModel:
             ]
         )
 
+        # Updata every cell
         for y in range(self.height):
             for x in range(self.width):
                 self.muscle_fiber_grid[y][x].perform_daily_activity()
